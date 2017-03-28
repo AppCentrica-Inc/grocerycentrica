ff294ce12653a82d17c1dba02b8ba970cfb643c5
Henrique Cabral <henrique.cabral@appcentrica.com>
2017-03-28 17:21:31 -0400

    Change layout login

--- imports/ui/DisplayImage.jsx
+++ imports/ui/DisplayImage.jsx
@@ -4,7 +4,8 @@ import React, { Component } from 'react';
 export default class DisplayAnImage extends Component {
   render() {
       return (<div className='loginbox'> 
-            <img className='photoLayout' src={this.props.profilePhoto} /><span>{this.props.profileName}</span>
+        {this.props.profileName}
+            <img className='photoLayout' src={this.props.profilePhoto} />
       </div>);
   }
 }
